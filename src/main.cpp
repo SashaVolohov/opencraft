4db4293ad9daede527d4c4e99715f304a7be196c
SashaVolohov <sashavolohov20077411@yandex.ru>
2022-12-12 14:06:38 +0300

    GLFW added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
-﻿#include <iostream>
+﻿#include <GLFW/glfw3.h>
 
-int main(int argc, char* argv[])
+int main(void)
 {
-    std::cout << "Hello World" << std::endl;
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        //glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
